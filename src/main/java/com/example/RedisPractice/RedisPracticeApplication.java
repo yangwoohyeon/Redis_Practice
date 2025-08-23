2b9cc81197211a37ede912f6252a1ed526ddf6d9
yang-woohyeon <dngus0717@gmail.com>
2025-08-23 17:19:30 +0900

    @EnableJpaAuditing 추가

--- src/main/java/com/example/RedisPractice/RedisPracticeApplication.java
+++ src/main/java/com/example/RedisPractice/RedisPracticeApplication.java
@@ -2,8 +2,10 @@ package com.example.RedisPractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RedisPracticeApplication {
 
 	public static void main(String[] args) {
