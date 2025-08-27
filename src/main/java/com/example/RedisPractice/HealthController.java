93442965839e1885e548a8fe7b34e6104768dbc7
yang-woohyeon <dngus0717@gmail.com>
2025-08-27 15:50:59 +0900

    헬스체크 api

--- src/main/java/com/example/RedisPractice/HealthController.java
+++ src/main/java/com/example/RedisPractice/HealthController.java
@@ -1,11 +1,13 @@
 package com.example.RedisPractice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class HealthController {
 
     @GetMapping("/")
